882d219400d7e41e6572faae9f94f1c6d5419012
Snow-Dancing <2459274972@qq.com>
2019-06-09 11:01:19 +0800

    检查下载的图片是否完整

--- pixivSpider.py
+++ pixivSpider.py
@@ -1,4 +1,7 @@
 # -*- coding=utf-8 -*-
+# author : Zhang Chizhan
+# date : 2019/6/8
+# func: pixiv Spider
 
 import requests as rq
 import re
@@ -38,6 +41,7 @@ class MyPixivCrawler(object):
             self.setting.total_download = int(r[2])         # 历史下载的图片总数
             self.setting.lenpage = int(r[3])               # 检索结果的页面总数
             self.setting.total_find_pictures = int(r[4])    # 检索出的图片总数
+            self.setting.finished = int(r[5])              # 爬虫是否工作完成
 
     # 登录p站
     def login(self):
@@ -96,7 +100,7 @@ class MyPixivCrawler(object):
             return r.text
         except:
             # 若捕获异常说明图片主页信息访问失败,保存爬虫记录并返回空字符串
-            self.setting.logInfo("单张图片信息网页访问异常！可能网络连接已中断！")
+            self.setting.logInfo("单张图片信息主页访问异常！可能网络连接已中断！")
             self.saveSetting()
             return ""
 
@@ -111,7 +115,7 @@ class MyPixivCrawler(object):
         except:
             self.setting.logInfo("搜索结果网页访问异常！可能网络连接已中断！")
             self.saveSetting()
-            sys._exit(0)
+            sys.exit(0)
         # 配置检索本页图片链接的正则表达式格式
         url_form = re.compile(r'https:\\/\\/i.pximg.net\\/c\\/240x240\\/img-master\\/img\\/\d+\\/\d+\\/\d+\\/\d+\\/\d+\\/\d+\\/\d+_p0_master1200.jpg')
         # 根据正则表达式提取出本页所有符合格式的图片链接，列表形式:[url1, url2,]
@@ -173,46 +177,62 @@ class MyPixivCrawler(object):
         user_agent_list = self.setting.user_agent_list
         user_agent = random.choice(user_agent_list)
         figname = self.setting.fig + url[45:-15]
-        # 依次尝试访问以jpg, png和gif为后缀的地址
-        figurl = figname + '.jpg'
-        header = {'User-Agent' : user_agent, 'Referer' : refer}
-        try:
-            r = self.session.get(figurl, headers = header, timeout = self.setting.timeout)  # 连接超时时间设置
-            r.raise_for_status()
-        except:
-            figurl = figname + '.png'
+        # 如果下载图片不完整则重新下载图片
+        while True:
+            # 依次尝试访问以jpg, png和gif为后缀的地址
+            figurl = figname + '.jpg'
+            header = {'User-Agent' : user_agent, 'Referer' : refer}
             try:
                 r = self.session.get(figurl, headers = header, timeout = self.setting.timeout)  # 连接超时时间设置
                 r.raise_for_status()
             except:
-                figurl = figname + '.gif'
+                figurl = figname + '.png'
                 try:
                     r = self.session.get(figurl, headers = header, timeout = self.setting.timeout)  # 连接超时时间设置
                     r.raise_for_status()
                 except:
-                    # TODO 若以上三种后缀都不对,则可能下载图片过程中S出错,或者是其它后缀,此处可能会出bug
-                    self.setting.logInfo("图片下载异常！")
-                    # print(refer)
-                    self.saveSetting()
-                    return False
-        # 图片名字为likecount + 图片id + 后缀;  filename = 路径 + 图片名字
-        filename = os.path.join(self.setting.result_dir, str(likecount) + '_' + id + figurl[-4:])
-        # 在保存一张图片时,用线程锁锁上,防止保存出错
-        with self.setting.lock:
-            # 保存图片
-            with open(filename, 'wb') as f:
-                f.write(r.content)
-        # 从待处理的图片链接字典中删除本图片,更新并保存记录信息
-        self.setting.logInfo("成功下载一张图片: " + filename + "  喜爱数为: " + str(likecount))
-        self.setting.total_download += 1
-        try:
-            self.setting.urls_waitting.pop(url)
-        except:
-            pass
-        # 把刚下载的图片保存到已下载的图片字典中,格式为{picture1_id : likecount1, picture2_id : likecount2}
-        self.setting.picture_dic[id] = likecount
-        self.saveSetting()
-        return True
+                    figurl = figname + '.gif'
+                    try:
+                        r = self.session.get(figurl, headers = header, timeout = self.setting.timeout)  # 连接超时时间设置
+                        r.raise_for_status()
+                    except:
+                        # TODO 若以上三种后缀都不对,则可能下载图片过程中网络中断,或者是其它后缀,此处可能会出bug
+                        self.setting.logInfo("图片下载异常！可能网络连接已中断！")
+                        # print(refer)
+                        self.saveSetting()
+                        return False
+            pictureType = self.checkPicture(r.content)
+            if pictureType:     # 如果下载图片完整
+                # 图片名字为likecount + 图片id + 后缀;  filename = 路径 + 图片名字
+                filename = os.path.join(self.setting.result_dir, str(likecount) + '_' + id + pictureType)
+                # 保存图片
+                with open(filename, 'wb') as f:
+                    f.write(r.content)
+                # 从待处理的图片链接字典中删除本图片,更新并保存记录信息
+                self.setting.logInfo("成功下载一张图片: " + filename + "  喜爱数为: " + str(likecount))
+                self.setting.total_download += 1
+                try:
+                    self.setting.urls_waitting.pop(url)
+                except:
+                    pass
+                # 把刚下载的图片保存到已下载的图片字典中,格式为{picture1_id : likecount1, picture2_id : likecount2}
+                self.setting.picture_dic[id] = likecount
+                self.saveSetting()
+                return True
+            self.setting.logInfo("下载的图片不完整,正在重新下载！")
+            time.sleep(1)       # 休眠1s再重新访问，防止访问太频繁被识别出来是爬虫
+
+    # 检查服务器返回的二进制文件是否完整,若完整，返回图片类型，否则返回空字符串
+    def checkPicture(self, bitfile):
+        if bytes([bitfile[-1]]) == b";":
+            return '.gif'
+        elif bytes([bitfile[-1]]) == b'\xd9':
+            if bytes([bitfile[-2]]) == b'\xff':
+                return '.jpg'
+        elif bytes([bitfile[-1]]) == b'\x82':
+            if bytes([bitfile[-2]]) == b'`':
+                return '.png'
+        return ""
 
     # 保存当前状态
     def saveSetting(self):
@@ -224,13 +244,14 @@ class MyPixivCrawler(object):
             # 存储已经下载的图片   字典格式{picture1_id : likecount1, picture2_id : likecount2}
             with open(self.setting.pic_dic_path, 'w', encoding='utf8') as f:
                 json.dump(self.setting.picture_dic, f, ensure_ascii=False)
-            # 存储运行状态参数(当前页面,总浏览数,总下载数,检索结果页面总数，检索出的图片总数)
+            # 存储运行状态参数(当前页面,总浏览数,总下载数,检索结果页面总数，检索出的图片总数,完成状态)
             with open(self.setting.pagenum_path, 'w', encoding='utf8') as f:
                 f.write(str(self.setting.pagenum) + " " +
                         str(self.setting.total_browsed_pictures) + " " +
                         str(self.setting.total_download) + " " +
                         str(self.setting.lenpage) + " " +
-                        str(self.setting.total_find_pictures))
+                        str(self.setting.total_find_pictures) + " " +
+                        str(self.setting.finished))
             self.setting.logInfo("当前爬取状态已成功保存!")
 
     # 对每一页的图片链接列表挨个爬取
@@ -238,7 +259,7 @@ class MyPixivCrawler(object):
         length = len(url_list)      # 列表中链接总个数
         for i,url in enumerate(url_list):   # 对列表中每一个链接
             # 以下循环监听爬虫状态 以及 等待分配可用线程
-            while(True):
+            while True:
                 # 如果爬虫异常终止 并且 所有线程都运行结束
                 if not self.isrunning and not self.setting.working_thread:
                     sys.exit(0)     # 退出爬虫并抛出异常
@@ -269,16 +290,19 @@ class MyPixivCrawler(object):
         if self.setting.urls_waitting:  # 如果历史记录中有未访问的图片网页，则优先处理这些
             self.crawl_url_list(list(self.setting.urls_waitting.keys()))    # 爬取历史记录中未处理的网页
             self.setting.pagenum += 1            # 本页爬完，准备访问下一页
-            self.saveSetting()
+            self.saveSetting()              # 存档
         while self.setting.pagenum <= self.setting.lenpage:      # 若爬完所有页面，则终止
             word = "%20".join(self.setting.keyword.split())     # 多个检索关键词用%20拼接
             page_url = self.setting.website + word + self.setting.page + str(self.setting.pagenum)  # 得到检索结果某一页的地址
-            # page_url = self.setting.website + self.setting.keyword + self.setting.page + str(pagenum)
             url_list = self.getURL_fromPage(page_url)       # 爬取检索结果的某页，并从页面中提取出所有图片(约39或40个，视频跳过不管)
             self.saveSetting()                  # 每访问一次新的页面后保存记录
             self.crawl_url_list(url_list)       # 爬取该页的所有图片
             self.setting.pagenum += 1                        # 本页爬完，准备访问下一页
-        # 爬完所有网页，保存状态并退出
+        # 等待线程都运行结束，若爬完所有网页，将self.finished改为True,保存状态并退出
+        while self.setting.working_thread:
+            time.sleep(self.setting.sleep)
+        if self.isrunning:
+            self.setting.finished = 1
         self.saveSetting()
         sys.exit(0)
 
@@ -289,16 +313,17 @@ if __name__ == '__main__':
     # 是否初始化
     if setting.cleanDir:  # 若初始化
         setting.clean()  # 执行初始化
-    setting.logInfo("开始爬取！搜索关键词为：" + setting.keyword)
+    setting.logInfo(("开始爬取！搜索关键词为：" + setting.keyword +
+                     " | 下载阈值为: %d | 总线程数为: %d") % (setting.low, setting.max_thread))
     setting.logInfo( "正在尝试第一次登录...")
     # 循环监听
-    while(True):
+    while True:
         crawler = MyPixivCrawler()  # 生成爬虫
         try:
             crawler.run()       # 执行爬虫，若登录失败或者运行过程中连接中断会抛出异常
         except: # 捕获异常
             # 若是爬虫运行结束正常退出，则关闭该程序
-            if crawler.setting.pagenum > crawler.setting.lenpage:
+            if crawler.setting.finished:
                 setting.logInfo("爬取结束！成功爬取到%d张图片!" % crawler.setting.total_download)
                 break
             # 若因为网络连接中断非正常退出则休眠5s再尝试重新登录
